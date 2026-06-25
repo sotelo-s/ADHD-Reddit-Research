ab825809bdc948efded09d767c77006424942254
sotelo-s <s.sotelopenedo@gmail.com>
2026-06-25 19:55:27 +0200

    Added log creation for blurred images

--- code/data_processing/upload_preparation.py
+++ code/data_processing/upload_preparation.py
@@ -1,7 +1,7 @@
 '''
     Removes broken and NSFW images, removes empty content with no images and censores faces.
     
-    Use: python upload_preparation.py <user_data.csv> <content_data.csv> <media_folder> <NSFW_media_list.csv>
+    Use: python upload_preparation.py <user_data_out.csv> <content_data_out.csv> <user_data.csv> <content_data.csv> <media_folder> <NSFW_media_list.csv>
 '''
 
 import pandas as pd
@@ -10,6 +10,7 @@ import os
 from media_anonymizer import clean_media
 from data_cleaning import fix
 from PIL import Image
+import csv
 
 def delete_broken_and_nsfw(media_folder,nsfw_file):
     nsfw_dict = {}
@@ -53,14 +54,16 @@ def delete_broken_and_nsfw(media_folder,nsfw_file):
      
 
 if __name__ == "__main__":
-    if len(sys.argv) < 5:
-        print("Use: python upload_preparation.py <user_data.csv> <content_data.csv> <media_folder> <NSFW_media_list.csv>")
+    if len(sys.argv) < 7:
+        print("Use: python upload_preparation.py <user_data_out.csv> <content_data_out.csv> <user_data.csv> <content_data.csv> <media_folder> <NSFW_media_list.csv>")
         sys.exit(1)
         
-    ud_file = sys.argv[1]
-    cd_file = sys.argv[2]
-    media_folder = sys.argv[3]
-    nsfw_file = sys.argv[4]
+    ud_file_out = sys.argv[1]
+    cd_file_out = sys.argv[2]
+    ud_file = sys.argv[3]
+    cd_file = sys.argv[4]
+    media_folder = sys.argv[5]
+    nsfw_file = sys.argv[6]
     
     delete_broken_and_nsfw(media_folder,nsfw_file)
     
@@ -68,9 +71,16 @@ if __name__ == "__main__":
     content_df = pd.read_csv(cd_file)
     
     users_df, content_df = fix(users_df,content_df,media_folder)
-    clean_media(media_folder,blur=True)
+    total,mod, images = clean_media(media_folder, blur=True)
     
-    users_df.to_csv(ud_file,index=False)
-    content_df.to_csv(cd_file,index=False)
+    
+    with open("images_with_faces.csv", 'w', newline='', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, fieldnames=["content_id","image"])
+        writer.writeheader()
+        for exp in images:
+            writer.writerow(exp)
+    
+    users_df.to_csv(ud_file_out,index=False)
+    content_df.to_csv(cd_file_out,index=False)
     
     
